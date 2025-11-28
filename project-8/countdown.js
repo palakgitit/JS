4691fd4ececf0f92b976ff5e9eb34a249e6b12d2
palakgitit <palkzzzy@gmail.com>
2025-11-28 10:43:46 +0530

    commit

--- project-8/countdown.js
+++ project-8/countdown.js
@@ -1,23 +1,46 @@
+let counterstartNUM = null;
+let paused = true;
 
-
-let counterstartNUM = prompt("Number to start with: ");
-document.getElementById("circle").innerHTML=counterstartNUM;
-
-
-// making funciton 
-
-function startBTN(){
-    
-    if (counterstartNUM == null){
-        return;
-    }
-    if(counterstartNUM <= 0){
-        alert(("Out!"));
-        return;
-    }
-    counterstartNUM--;
-    document.getElementById("circle").innerHTML=counterstartNUM;
+function startBTN() {
 
     
+    if (counterstartNUM === null) {
+        counterstartNUM = prompt("Number to start with:");
+        counterstartNUM = parseInt(counterstartNUM);
+
+        if (isNaN(counterstartNUM) || counterstartNUM <= 0) {
+            alert("Enter valid number");
+            counterstartNUM = null;
+            return;
+        }
+    }
+
+    paused = false;
+    countNow();
 }
 
+function countNow() {
+
+    if (paused) {
+        return;
+    }
+
+    if (counterstartNUM <= 0) {
+        return;
+    }
+
+    counterstartNUM--;
+    document.getElementById("circle").innerHTML = counterstartNUM;
+
+    setTimeout(countNow, 1000);
+}
+
+function pauseBTN() {
+    paused = true;
+}
+
+function resetBTN() {
+    paused = true;
+    counterstartNUM = null;
+    document.getElementById("circle").innerHTML = " ";
+}
