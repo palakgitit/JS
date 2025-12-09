9ce132bbea032e5547e61012b39d28827f2f38f4
palakgitit <palkzzzy@gmail.com>
2025-12-09 10:41:28 +0530

    commit

--- project-9/js/cart.js
+++ project-9/js/cart.js
@@ -40,8 +40,8 @@ function viewData() {
                     <h5 class="card-title">${p.name}</h5>
                     <p class="card-text">${p.desc}</p>
                     <p>₹${p.price}</p>
-                    <button class="btn btn-dark" onclick="addCart(id)">Add to Cart</button>
-                    <button class="btn btn-outline-dark" onclick="viewDetail(id)">View</button>
+                    <button class="btn btn-dark" onclick="addCart(${p.id})">Add to Cart</button>
+                    <button class="btn btn-outline-dark" onclick="viewDetail(p.id)">View</button>
                 </div>
             </div>
             </div>
@@ -51,10 +51,72 @@ function viewData() {
     document.getElementById("p-container").innerHTML = box;
 }
 
+let productCart = [];
+
+function addCart(id) {
 
 
-function emptyCart(){
-    localStorage.clear();
+    let products = JSON.parse(localStorage.getItem("products")) || [];
+    let item = products.find(p => p.id == id);
+    let exist = productCart.find(p => p.id == id);
+
+    if (exist) {
+        alert("Already in cart");
+        return;
+    }
+
+    productCart.push(item);
+    localStorage.setItem("cart", JSON.stringify(productCart));
+    console.log(productCart);
+    alert("Added to cart!");
+    viewCart();
+}
+
+
+
+function viewCart() {
+    let productCart = JSON.parse(localStorage.getItem("cart")) || [];
+    let section = `
+        <table class="table">
+            <tr>
+                <th>Image</th>
+                <th>Product Name</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Total</th>
+                <th>Action</th>
+            </tr>
+    `;
+
+    productCart.forEach(p => {
+        section += `
+            <tr>
+                <td><img src="${p.img}" style="width:50px;height:50px;"></td>
+                <td>${p.name}</td>
+                <td>₹${p.price}</td>
+                <td>${p.qty}</td>
+                <td>₹${p.price * p.qty}</td>
+                <td><button onclick="deleteItem('${p.id}')">Delete</button></td>
+            </tr>
+        `;
+    });
+
+    section += `</table>`;
+    document.getElementById("displayCart").innerHTML = section;
+}
+
+function deleteItem(id) {
+    let productCart = JSON.parse(localStorage.getItem("cart")) || [];
+    productCart = productCart.filter(p => p.id != id);
+    localStorage.setItem("cart", JSON.stringify(productCart));
+    viewCart();
+}
+
+
+
+function emptyCart() {
+    localStorage.removeItem("cart");
+    viewCart();
 }
 
 
